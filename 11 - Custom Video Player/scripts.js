fa6bc12e631cadd390601c35c7ce6ec5e3ab77c8
Abdul Momin <abdulmomin114477@gmail.com>
2020-10-02 17:11:50 +0600

    all

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,16 +1,59 @@
-const video = document.querySelector("video");
+const video = document.querySelector(".player__video");
+const progress = document.querySelector(".progress");
+const progressFilled = document.querySelector(".progress__filled");
 const playPause = document.querySelector(".toggle");
 const volume = document.querySelector("input[name=volume]");
-const rate = document.querySelector("input[name=playbackRate]");
+const speed = document.querySelector("input[name=playbackRate]");
 const forward = document.querySelector("button:nth-of-type(3)");
 const backward = document.querySelector("button:nth-of-type(2)");
 
-video.onclick = () => {
+let isMouseDown;
+
+const ctrlVolume = () => {
+  if (!isMouseDown) return;
+
+  const inputVolume = volume.value;
+  video.volume = inputVolume;
+
   console.dir(video);
 };
 
-playPause.onclick = () => {
-  video.paused = false;
-  video.seeking = true;
-  console.dir(video.paused, video.seeking);
+const ctrlSpeed = () => {
+  if (!isMouseDown) return;
+
+  const inputSpeed = speed.value;
+  video.playbackRate = inputSpeed;
 };
+
+const ctrlProgress = (e) => {
+  if (!isMouseDown) return;
+
+  console.log(e);
+};
+
+// Duration bar
+progress.addEventListener("mousedown", () => {
+  isMouseDown = true;
+  ctrlProgress();
+});
+progress.addEventListener("mouseup", () => (isMouseDown = false));
+progress.addEventListener("mousemove", ctrlProgress);
+
+// play pause
+playPause.onclick = () => (video.paused ? video.play() : video.pause());
+
+// volume
+volume.addEventListener("mousedown", () => {
+  isMouseDown = true;
+  ctrlVolume();
+});
+volume.addEventListener("mouseup", () => (isMouseDown = false));
+volume.addEventListener("mousemove", ctrlVolume);
+
+// speed / playbackRate
+speed.addEventListener("mousedown", () => {
+  isMouseDown = true;
+  ctrlSpeed();
+});
+speed.addEventListener("mouseup", () => (isMouseDown = false));
+speed.addEventListener("mousemove", ctrlSpeed);
