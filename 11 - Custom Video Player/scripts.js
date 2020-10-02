ff9da35c5aa8f87304abafcd2e610618ebb30929
Abdul Momin <abdulmomin114477@gmail.com>
2020-10-02 17:49:00 +0600

    all

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -4,10 +4,22 @@ const progressFilled = document.querySelector(".progress__filled");
 const playPause = document.querySelector(".toggle");
 const volume = document.querySelector("input[name=volume]");
 const speed = document.querySelector("input[name=playbackRate]");
-const forward = document.querySelector("button:nth-of-type(3)");
-const backward = document.querySelector("button:nth-of-type(2)");
+const buttons = [
+  document.querySelector("button:nth-of-type(3)"),
+  document.querySelector("button:nth-of-type(2)"),
+];
 
 let isMouseDown;
+
+const togglePlay = () => {
+  if (!video.paused) {
+    playPause.innerHTML = "►";
+    video.pause();
+  } else {
+    playPause.innerHTML = "❚❚";
+    video.play();
+  }
+};
 
 const ctrlVolume = () => {
   if (!isMouseDown) return;
@@ -31,6 +43,10 @@ const ctrlProgress = (e) => {
   console.log(e);
 };
 
+const ctrlSkip = (e) => {
+  video.currentTime += parseFloat(e.target.dataset.skip);
+};
+
 // Duration bar
 progress.addEventListener("mousedown", () => {
   isMouseDown = true;
@@ -40,8 +56,8 @@ progress.addEventListener("mouseup", () => (isMouseDown = false));
 progress.addEventListener("mousemove", ctrlProgress);
 
 // play pause
-playPause.onclick = () => (video.paused ? video.play() : video.pause());
-
+playPause.onclick = () => togglePlay();
+video.onclick = () => togglePlay();
 // volume
 volume.addEventListener("mousedown", () => {
   isMouseDown = true;
@@ -57,3 +73,10 @@ speed.addEventListener("mousedown", () => {
 });
 speed.addEventListener("mouseup", () => (isMouseDown = false));
 speed.addEventListener("mousemove", ctrlSpeed);
+
+// skip
+buttons.forEach((button) => button.addEventListener("click", ctrlSkip));
+
+// setInterval(() => {
+//   console.log(video.currentTime);
+// }, 1000);
