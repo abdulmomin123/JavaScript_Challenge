ecf7a587317c90be699b7a7f7297c08ff8e205f8
Abdul Momin <abdulmomin114477@gmail.com>
2020-10-07 22:06:17 +0600

    all

--- 18 - Adding Up Times with Reduce/app.js
+++ 18 - Adding Up Times with Reduce/app.js
@@ -1,7 +1,9 @@
 const times = [...document.querySelectorAll("li")].map((el) => el.dataset.time);
 
 const totalTime = times.reduce((acc, cur) => {
-  //
+  const minuteSecond = cur.split(":");
+
+  return acc + parseInt(minuteSecond[0]);
 }, 0);
 
-console.log(times);
+console.log(times, totalTime);
