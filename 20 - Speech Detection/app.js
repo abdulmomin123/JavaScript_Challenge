5f3a7e5008714cfa8a62384269b812be539c72d9
Abdul Momin <abdulmomin114477@gmail.com>
2020-10-09 06:21:00 +0600

    all

--- 20 - Speech Detection/app.js
+++ 20 - Speech Detection/app.js
@@ -13,3 +13,4 @@ recognition.addEventListener("result", (e) => {
   const transcript = [...e.results].map((el) => el[0].transcript).join();
   console.log(transcript);
 });
+recognition.addEventListener("end", recognition.start);
