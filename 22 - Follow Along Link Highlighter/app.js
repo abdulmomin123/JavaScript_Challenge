16e25f2de86ffb5ae48155de21bdf02432cfc04e
Abdul Momin <abdulmomin114477@gmail.com>
2020-10-10 16:58:46 +0600

    all

--- 22 - Follow Along Link Highlighter/app.js
+++ 22 - Follow Along Link Highlighter/app.js
@@ -1,14 +1,13 @@
-const hl = document.querySelector(".hl");
+const hl = document.querySelector(".highlight");
 const links = document.querySelectorAll('[href=""]');
 
 links.forEach((link) =>
   link.addEventListener("mouseenter", function (e) {
-    hl.style.width = `${this.getBoundingClientRect().width}px`;
-    hl.style.height = `${this.getBoundingClientRect().height}px`;
-    hl.style.transform = `translateX(${this.getBoundingClientRect().left}px)`;
-    hl.style.transform = `translate(${this.getBoundingClientRect().left}px, ${
-      this.getBoundingClientRect().top
-    }px)`;
+    const dimentions = this.getBoundingClientRect();
+    hl.style.width = `${dimentions.width}px`;
+    hl.style.height = `${dimentions.height}px`;
+    hl.style.transform = `translateX(${dimentions.left}px)`;
+    hl.style.transform = `translate(${dimentions.left}px, ${dimentions.top}px)`;
 
     console.log(this.getBoundingClientRect());
   })
