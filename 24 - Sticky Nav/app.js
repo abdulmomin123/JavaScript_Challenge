d452f546e601569f92a8d63c55ea6822ba6be11c
Abdul Momin <abdulmomin114477@gmail.com>
2020-10-11 15:08:18 +0600

    all

--- 24 - Sticky Nav/app.js
+++ 24 - Sticky Nav/app.js
@@ -1,7 +1,17 @@
 const body = document.querySelector("body");
+const nav = document.querySelector("#main");
 
-// body.classList.add("fixed-nav");
 function toggleClass(e) {
-  //
+  const navTop = nav.offsetTop;
+
+  if (window.scrollY >= navTop) {
+    body.style.paddingTop = `${nav.offsetHeight}px`;
+    body.classList.add("fixed-nav");
+  } else {
+    body.style.paddingTop = 0;
+    body.classList.remove("fixed-nav");
+  }
+  console.log(navTop);
 }
+
 window.addEventListener("scroll", toggleClass);
