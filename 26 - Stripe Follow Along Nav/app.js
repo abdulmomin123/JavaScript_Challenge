76f2e8143e93e399d8d538a39e707bd10adc1952
Abdul Momin <abdulmomin114477@gmail.com>
2020-10-12 17:19:14 +0600

    all

--- 26 - Stripe Follow Along Nav/app.js
+++ 26 - Stripe Follow Along Nav/app.js
@@ -6,17 +6,24 @@ function displayBG(e) {
   const dimentions = this.querySelector(".dropdown").getBoundingClientRect();
   const dimentions2 = this.querySelector("a").getBoundingClientRect();
 
+  bg.style.display = "flex";
+  this.querySelector(".dropdown").style.transition = "all 0.5s 0.1s";
   bg.style.width = `${dimentions.width}px`;
   bg.style.height = `${dimentions.height}px`;
   bg.style.opacity = 1;
-  bg.style.transform = `translate(${dimentions2.left}px, ${dimentions2.top}px)`;
+  bg.style.transform = `translate(${
+    dimentions2.left - dimentions.width / 2 + dimentions2.width / 2
+  }px, ${dimentions2.top}px)`;
 
   this.querySelector(".dropdown").style.opacity = 1;
   this.querySelector(".dropdown").style.visibility = "visible";
 }
 
 function removeBG(e) {
-  console.log(e);
+  this.querySelector(".dropdown").style.transition = "none";
+  this.querySelector(".dropdown").style.visibility = "hidden";
+
+  // bg.style.display = "none";
 }
 
 links.forEach((link) => link.addEventListener("mouseenter", displayBG));
