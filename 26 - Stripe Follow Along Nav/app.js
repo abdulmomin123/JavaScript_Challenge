112fdf98fff126bc587e7dc7db20c7882b6f5d0d
Abdul Momin <abdulmomin114477@gmail.com>
2020-10-12 16:15:00 +0600

    all

--- 26 - Stripe Follow Along Nav/app.js
+++ 26 - Stripe Follow Along Nav/app.js
@@ -4,13 +4,15 @@ const bg = document.querySelector(".dropdownBackground");
 
 function displayBG(e) {
   const dimentions = this.querySelector(".dropdown").getBoundingClientRect();
+  const dimentions2 = this.querySelector("a").getBoundingClientRect();
 
   bg.style.width = `${dimentions.width}px`;
   bg.style.height = `${dimentions.height}px`;
   bg.style.opacity = 1;
-  bg.style.transform = `translate(${dimentions.left}px, ${dimentions.top}px)`;
+  bg.style.transform = `translate(${dimentions2.left}px, ${dimentions2.top}px)`;
 
   this.querySelector(".dropdown").style.opacity = 1;
+  this.querySelector(".dropdown").style.visibility = "visible";
 }
 
 function removeBG(e) {
