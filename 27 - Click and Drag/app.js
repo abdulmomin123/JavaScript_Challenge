c732a39f81e48aca5d6c8dd8cafa1c0452d4b442
Abdul Momin <abdulmomin114477@gmail.com>
2020-10-12 22:42:32 +0600

    all

--- 27 - Click and Drag/app.js
+++ 27 - Click and Drag/app.js
@@ -1,21 +1,21 @@
 const div = document.querySelector(".items");
 
 let isMouseDown = false;
-let draggedPx;
+let draggedPx = 0;
 let lastMousePos;
 
 function scroll(e) {
   if (!isMouseDown) return;
 
   lastMousePos = e.pageX;
-  draggedPx = lastMousePos - draggedPx;
+  draggedPx += 3;
 
   div.scrollLeft = draggedPx;
   console.log(draggedPx);
 }
 
 div.addEventListener("mousedown", (e) => {
-  [isMouseDown, draggedPx] = [true, e.pageX];
+  [isMouseDown, draggedPx] = [true, e.pageX - div.offsetLeft];
   div.classList.add("active");
 });
 div.addEventListener("mouseup", () => {
