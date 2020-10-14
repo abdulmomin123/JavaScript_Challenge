c6242167f972e9d338fb5571d5b57cf2d973c781
Abdul Momin <abdulmomin114477@gmail.com>
2020-10-14 18:20:23 +0600

    all

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,14 +1,17 @@
-let time;
+let countdown;
 
 function count(sec) {
   const now = Date.now();
   const then = now + sec * 1000;
 
-  setInterval(() => {
+  countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
+
+    if (secondsLeft <= 0) clearInterval(countdown);
+
     console.log(secondsLeft);
   }, 1000);
 
   // console.log({ now, then });
 }
-// count(10);
+count(3);
