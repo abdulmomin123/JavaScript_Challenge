7c626c3b4ea6cc792c44b8eb160aa94455c91070
Abdul Momin <abdulmomin114477@gmail.com>
2020-10-15 21:52:41 +0600

    all

--- 30 - Whack A Mole/app.js
+++ 30 - Whack A Mole/app.js
@@ -1,39 +1,48 @@
-const holes = [...document.querySelectorAll(".hole")];
+const holes = document.querySelectorAll(".hole");
 const scoreBoard = document.querySelector(".score");
 const moles = document.querySelectorAll(".mole");
-
+let lastHole;
+let timeUp = false;
 let score = 0;
-let timer;
+
+function randomTime(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+function randomHole(holes) {
+  const idx = Math.floor(Math.random() * holes.length);
+  const hole = holes[idx];
+  if (hole === lastHole) {
+    console.log("Ah nah thats the same one bud");
+    return randomHole(holes);
+  }
+  lastHole = hole;
+  return hole;
+}
+
+function peep() {
+  const time = randomTime(200, 1000);
+  const hole = randomHole(holes);
+  hole.classList.add("up");
+  setTimeout(() => {
+    hole.classList.remove("up");
+    if (!timeUp) peep();
+  }, time);
+}
 
 function startGame() {
-  clearInterval(timer);
-
-  timer = setInterval(() => {
-    holes.forEach((hole) => {
-      if (hole.classList.contains("up")) {
-        hole.classList.remove("up");
-      }
-    });
-    const random = Math.floor(Math.random() * 6);
-
-    holes[random].classList.add("up");
-  }, 500);
-
-  setTimeout(() => {
-    clearInterval(timer);
-  }, 10000);
-
-  moles.forEach((mole) => mole.addEventListener("click", addScore));
+  scoreBoard.textContent = 0;
+  timeUp = false;
+  score = 0;
+  peep();
+  setTimeout(() => (timeUp = true), 10000);
 }
 
-function addScore() {
-  holes.forEach((hole) => {
-    if (hole.classList.contains("up")) {
-      hole.classList.remove("up");
-    }
-  });
+function bonk(e) {
+  if (!e.isTrusted) return; // cheater!
   score++;
-
-  //  update the score
+  this.parentNode.classList.remove("up");
   scoreBoard.textContent = score;
 }
+
+moles.forEach((mole) => mole.addEventListener("click", bonk));
