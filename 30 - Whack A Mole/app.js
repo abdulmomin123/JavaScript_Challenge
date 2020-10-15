227afde1829aca8b7255967791a05b09766c1b58
Abdul Momin <abdulmomin114477@gmail.com>
2020-10-15 17:27:46 +0600

    all

--- 30 - Whack A Mole/app.js
+++ 30 - Whack A Mole/app.js
@@ -2,8 +2,13 @@ const holes = [...document.querySelectorAll(".hole")];
 const scoreBoard = document.querySelector(".score");
 const moles = document.querySelectorAll(".mole");
 
+let score = 0;
+let timer;
+
 function startGame(e) {
-  const timer = setInterval(() => {
+  clearInterval(timer);
+
+  timer = setInterval(() => {
     holes.forEach((hole) => {
       if (hole.classList.contains("up")) {
         hole.classList.remove("up");
@@ -12,5 +17,19 @@ function startGame(e) {
     const random = Math.floor(Math.random() * 6);
 
     holes[random].classList.add("up");
+
+    //  update the score
+    scoreBoard.textContent = score;
   }, 500);
+
+  moles.forEach((mole) => mole.addEventListener("click", addScore));
+}
+
+function addScore(e) {
+  holes.forEach((hole) => {
+    if (hole.classList.contains("up")) {
+      hole.classList.remove("up");
+    }
+  });
+  score++;
 }
